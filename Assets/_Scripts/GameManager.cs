fa15654f08d54a1b790ab792a59199644a89eac6
Costa <costaanton96@gmail.com>
2026-01-30 13:05:05 +0200

    feat: add placeholder buttons to main menu

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace GGJ.Managers
 {
-    public class GameManager
+    public class GameManager : MonoBehaviour
     {
-        
+        [SerializeField] private Button _startButton;
+        [SerializeField] private Button _exitButton;
     }
 }
